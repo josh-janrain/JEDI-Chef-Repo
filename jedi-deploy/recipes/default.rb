d47ed35885d31e91703a88bc3c87b94b0e45e0f1
joshua@janrain.com <joshua@janrain.com>
2014-05-27 16:43:32 +0000

    changed permissions again

--- jedi-deploy/recipes/default.rb
+++ jedi-deploy/recipes/default.rb
@@ -47,7 +47,6 @@ template "/etc/opt/jedi/install.conf" do
 	source "install.conf.erb"
 	owner "jedi"
 	group "jedi"
-	mode "0700"
 	variables({
 		:install_home => "/srv/www/jedi/current",
 		:install_cwd => "/var/jedi",
